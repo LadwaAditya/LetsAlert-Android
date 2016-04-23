d2d244af7fadc554512968b35ca8896bc0cb055a
Aditya <ladwa.aditya@gmail.com>
2016-04-23 16:59:47 +0530

    fect: Added RxJava Support

--- app/src/main/java/com/adityaladwa/letsalert/SignUpActivity.java
+++ app/src/main/java/com/adityaladwa/letsalert/SignUpActivity.java
@@ -12,16 +12,21 @@ import com.adityaladwa.letsalert.api.model.Error;
 import com.adityaladwa.letsalert.api.model.People;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 
 import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.HttpException;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -64,26 +69,63 @@ public class SignUpActivity extends AppCompatActivity {
 
         Call<People> call = retrofit.create(EndPoint.class).signUp(people);
 
-        call.enqueue(new Callback<People>() {
-            @Override
-            public void onResponse(Call<People> call, Response<People> response) {
-                if (response.body() == null)
-                    try {
-                        Error error = (Error) retrofit.responseBodyConverter(Error.class, Error.class.getAnnotations()).convert(response.errorBody());
-                        Toast.makeText(SignUpActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+        retrofit.create(EndPoint.class).signUpRx(people).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(new Observer<People>() {
+                    @Override
+                    public void onCompleted() {
                         progressDialog.hide();
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
-                else
-                    Toast.makeText(SignUpActivity.this, response.body().toString(), Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(Call<People> call, Throwable t) {
-                Toast.makeText(SignUpActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+                    @Override
+                    public void onError(Throwable e) {
+                        progressDialog.hide();
+                        if (e instanceof HttpException) {
+                            //we have a HTTP exception (HTTP status code is not 200-300)
+                            Converter<ResponseBody, Error> errorConverter =
+                                    retrofit.responseBodyConverter(Error.class, new Annotation[0]);
+                            //maybe check if ((HttpException) throwable).code() == 400 ??
+                            try {
+                                Error error = errorConverter.convert(((HttpException) e).response().errorBody());
+                                Toast.makeText(SignUpActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                            } catch (IOException e1) {
+                                e1.printStackTrace();
+                            }
+                        }
+
+                    }
+
+                    @Override
+                    public void onNext(People people) {
+                        progressDialog.hide();
+                        Toast.makeText(SignUpActivity.this, people.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+//        call.enqueue(new Callback<People>() {
+//            @Override
+//            public void onResponse(Call<People> call, Response<People> response) {
+//                if (response.body() == null)
+//                    try {
+//                        Error error = (Error) retrofit.responseBodyConverter(Error.class, Error.class.getAnnotations()).convert(response.errorBody());
+//                        Toast.makeText(SignUpActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+//                        progressDialog.hide();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                else {
+//                    Toast.makeText(SignUpActivity.this, response.body().toString(), Toast.LENGTH_SHORT).show();
+//                    progressDialog.hide();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<People> call, Throwable t) {
+//                Toast.makeText(SignUpActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 }
--- app/src/main/java/com/adityaladwa/letsalert/api/EndPoint.java
+++ app/src/main/java/com/adityaladwa/letsalert/api/EndPoint.java
@@ -4,7 +4,9 @@ import com.adityaladwa.letsalert.api.model.People;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import rx.Observable;
 
 /**
  * Created by Aditya on 23-Apr-16.
@@ -12,4 +14,8 @@ import retrofit2.http.POST;
 public interface EndPoint {
     @POST("/api/people")
     Call<People> signUp(@Body People people);
+
+    @Headers("Content-Type: application/json")
+    @POST("/api/people")
+    Observable<People> signUpRx(@Body People people);
 }
